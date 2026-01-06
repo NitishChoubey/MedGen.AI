fa09d8e64b2c8e9afdb51c8d1210b3470a2e67c3
Nitish Choubey <nitishchoubey1234@gmail.com>
2026-01-06 19:12:19 +0530

    Doing some changes

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,15 @@
+import path from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-}
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
 
-export default nextConfig
+export default nextConfig;
+
+
+
+
