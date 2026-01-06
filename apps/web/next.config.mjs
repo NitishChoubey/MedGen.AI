9114211fcc6f46995038fc3686ce799ebd07347e
Nitish Choubey <nitishchoubey1234@gmail.com>
2026-01-06 19:23:45 +0530

    Chnaging next.config.mjs

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,11 @@
 import path from "path";
+import { fileURLToPath } from "url";
 
 /** @type {import('next').NextConfig} */
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
@@ -9,6 +14,7 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
 
 
 
